d8988ac5d54255c323bc50909de75ff87fd9853a
CoSat2023 <john.mcbride2768@gmail.com>
2023-03-22 17:33:13 -0700

    First attempt of exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringToRepeat = '';
+    if (num < 0) {
+        return("ERROR");
+    } else {
+        for (let i = 0; i < num; i++) {
+            stringToRepeat += string;
+        }
+    }
 
-};
+    return(stringToRepeat);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
